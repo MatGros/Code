ae7391b6dbd669bdbaedcf36cc2d7ff866915dd4
MatGros <mathieu.gros@gmail.com>
2022-08-08 18:19:33 +0200

    Add files via upload

---
+++ Python/Elia/Elia_essai comparaison.py
@@ -0,0 +1,14 @@
+print("Premier programe Elia")
+while True :
+    Two=input("premier nombre")
+    #print(Two)
+    Cupbear=input("deuxième nombre")
+    #print(Cupbear)
+    Result= int(Two) * int(Cupbear)
+    #Result= Two + Cupbear
+    print(Result)
+
+    if Result >= 100 :
+        Result
+
+
---
+++ Python/Elia/Elia_mon premier programme.py
@@ -0,0 +1,12 @@
+print("Premier programe Elia")
+while True :
+    Two=input("premier nombre")
+    #print(Two)
+    Cupbear=input("deuxième nombre")
+    #print(Cupbear)
+    Result= int(Two) * int(Cupbear)
+    #Result= Two + Cupbear
+    print(Result)
+
+
+
